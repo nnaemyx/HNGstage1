6f686cc444304914bc472d2f4a0ec4dcf4b5c17d
tezon <tezonteam@gmail.com>
2022-10-23 00:03:20 -0700

    project setup

---
+++ src/components/Profilesection/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Profileimg from '../../images/profile__img.png'
+
+const Profile = () => {
+  return (
+    <div>
+      <div>
+        <img src={Profileimg} alt=""/>
+      </div>
+    </div>
+  )
+}
+
+export default Profile

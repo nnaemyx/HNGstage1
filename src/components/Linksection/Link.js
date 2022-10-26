36bf8b80af8ea337071ada46c914bb069331662d
tezon <tezonteam@gmail.com>
2022-10-26 14:48:17 -0700

    changed my classname to id as instructed

--- src/components/Linksection/Link.js
+++ src/components/Linksection/Link.js
@@ -8,29 +8,29 @@ const Link = () => {
   return (
     <div className="link_section">
       <div className="links">
-        <button className="twitter">EdehJohnpaul</button>
+        <button id="twitter">EdehJohnpaul</button>
         <a href="https://training.zuri.team/" target="_blank" rel="noreferrer">
-          <button className="btn_zuri">Zuri Team</button>
+          <button id="btn_zuri">Zuri Team</button>
         </a>
         <a href="http://books.zuri.team" target="_blank" rel="noreferrer">
-          <button className="books">Zuri Books</button>
+          <button id="books">Zuri Books</button>
         </a>
         <a
           href="https://books.zuri.team/python-for-beginners?ref_id=edehjohnpaul"
           target="_blank"
           rel="noreferrer"
         >
-          <button className="book_python">Python Books</button>
+          <button id="book_python">Python Books</button>
         </a>
         <a href="https://background.zuri.team" target="_blank" rel="noreferrer">
-          <button className="pitch">Background Check for Coders</button>
+          <button id="pitch">Background Check for Coders</button>
         </a>
         <a
           href="https://books.zuri.team/design-rules"
           target="_blank"
           rel="noreferrer"
         >
-          <button className="book_design">Design Books</button>
+          <button id="book_design">Design Books</button>
         </a>
       </div>
       <div className="slackgit">
